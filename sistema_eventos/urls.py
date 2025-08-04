4c4fc22038d5dd558a067d9a3ed994050b6e0184
Bryan Moura <bryanmoura27@gmail.com>
2025-08-04 20:20:41 -0300

    sistema de eventos

--- sistema_eventos/urls.py
+++ sistema_eventos/urls.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
+
 from eventos.views import (
     EventoListView,
     EventoCreateView,
     EventoUpdateView,
     EventoDeleteView,
-    InscricaoCreateView
+    InscricaoCreateView,
 )
-from django.contrib.auth.views import LoginView, LogoutView
-
-# 👇 IMPORTANTE: essas duas linhas para arquivos de mídia
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,18 +19,16 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 
-    # Listagem de eventos
+    # Eventos
     path('', EventoListView.as_view(), name='evento-list'),
-
-    # CRUD de eventos
     path('evento/novo/', EventoCreateView.as_view(), name='evento-create'),
     path('evento/<int:pk>/editar/', EventoUpdateView.as_view(), name='evento-update'),
     path('evento/<int:pk>/deletar/', EventoDeleteView.as_view(), name='evento-delete'),
 
-    # Inscrição em evento
+    # Inscrição
     path('evento/<int:evento_id>/inscricao/', InscricaoCreateView.as_view(), name='evento-inscricao'),
 ]
 
-
+# Arquivos de mídia em modo de desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
